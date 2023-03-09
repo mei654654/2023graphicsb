64ad9ce7646f4c70ff4af1fd5a611ea7e61564fe
mei654654 <10160872@me.mcu.edu.tw>
2023-03-09 16:55:14 +0800

    week04

---
+++ week04_Translate/main.cpp
@@ -0,0 +1,23 @@
+#include <GL/glut.h>
+float angle = 0;///全域變數
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glPushMatrix();///備份矩陣
+
+        glRotated(angle, 0, 1, 0);
+        glutSolidTeapot( 0.3 );
+    glPopMatrix();///還原矩陣
+	glutSwapBuffers();
+	angle++; ///角度++
+}
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);///設定 GLUT 的顯示模式
+	glutCreateWindow("week04");
+
+	glutDisplayFunc(display); ///顯示的函式 display()
+	glutIdleFunc(display);
+	glutMainLoop(); ///最後要有 glutMainLoop(); 主要迴圈卡最後面
+}
