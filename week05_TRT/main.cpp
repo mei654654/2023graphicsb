e9b0ea186340dd3188b134ccdaceb0b731c9803d
mei654654 <10160872@me.mcu.edu.tw>
2023-03-16 16:24:26 +0800

    week05

---
+++ week05_TRT/main.cpp
@@ -0,0 +1,34 @@
+///旋轉軸改成0,0,1 Z軸,
+#include <GL/glut.h>
+float angle = 0;
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glColor3f(1, 0, 0);///紅色
+    glPushMatrix();
+        glRotated(angle, 0, 0, 1);///旋轉軸
+        glTranslatef(0.8, 0, 0);
+        glutSolidTeapot( 0.3 );
+    glPopMatrix();
+
+    glColor3f(0, 1, 0);///綠色
+    glPushMatrix();
+        glTranslatef(0.8, 0, 0);
+        glRotated(angle*1.5, 0, 0, 1);///旋轉軸,angle旋轉速度
+        glutSolidTeapot( 0.3 );
+    glPopMatrix();
+
+	glutSwapBuffers();
+	angle++;
+}
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutCreateWindow("week04");
+
+	glutDisplayFunc(display);
+	glutIdleFunc(display);
+	glutMainLoop();
+}
