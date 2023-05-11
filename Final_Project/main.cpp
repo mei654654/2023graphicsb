b806a9d66fe76fa50a1d0eb08d34a2efdceb89b9
mei654654 <10160872@me.mcu.edu.tw>
2023-05-11 16:11:58 +0800

    week13

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -1,15 +1,40 @@
 ///之後都用同一個程式來進行Final_project
 #include <stdio.h>
 #include <GL/glut.h>
+#include "glm.h"
+///把glm.cpp加入檔案
+GLMmodel * head = NULL;
+GLMmodel * body = NULL;
+GLMmodel * uparmR = NULL;
+GLMmodel * lowarmR = NULL;
+
+int show[4] = {1,0,0,0};
 float teapotX = 0, teapotY = 0;
 FILE * fout = NULL;
 FILE * fin = NULL;
+void keyboard(unsigned char key, int x, int y){
+    if(key=='0') show[0] =! show[0];
+    if(key=='1') show[1] =! show[1];
+    if(key=='2') show[2] =! show[2];
+    if(key=='3') show[3] =! show[3];
+    glutPostRedisplay();
+}
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    if(head==NULL){
+        head = glmReadOBJ("model/head.obj");
+        body = glmReadOBJ("model/body.obj");
+        uparmR = glmReadOBJ("model/rightarm.obj");
+        lowarmR = glmReadOBJ("model/righthand.obj");
+        ///glmUnitize(head);
+    }
     glPushMatrix();
-        glTranslatef(teapotX, teapotY, 0);
-        glutSolidTeapot( 0.3 );
+        glScalef(0.3, 0.3, 0.3);
+        if(show[0])glmDraw(head, GLM_MATERIAL);
+        if(show[1])glmDraw(body, GLM_MATERIAL);
+        if(show[2])glmDraw(uparmR, GLM_MATERIAL);
+        if(show[3])glmDraw(lowarmR, GLM_MATERIAL);
     glPopMatrix();
     glutSwapBuffers();
 }
@@ -24,7 +49,7 @@ void mouse(int button, int state, int x, int y)
     }
     display();
 }
-void keyboard(unsigned char key, int x, int y)
+/*void keyboard(unsigned char key, int x, int y)
 {
     if(fin==NULL){
         fclose(fout);
@@ -32,7 +57,7 @@ void keyboard(unsigned char key, int x, int y)
     }
     fscanf(fin, "%f%f", &teapotX, &teapotY);
     display();
-}
+}*/
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
